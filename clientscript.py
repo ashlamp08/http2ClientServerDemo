b0c2cc5c1e516095d2690cba3ec6bced12281dd1
AtteRouhe <atte.rouhe@gmail.com>
2022-02-17 16:51:52 +0200

    Add fourth step

--- clientscript.py
+++ clientscript.py
@@ -12,18 +12,20 @@ headers = [
 
 
 """
+2)
 Select a GPS location from the GPS trace we provide, and send a request for a regional road
 map from the HTTP client to the server. The GPS location is indicated in the request. The
 server should send the corresponding road map back to the client
 """
-requestBody = {"lat":60.170008599999, "lon":24.9389777}
+requestBody = {"lat": 60.17000859999999, "lon": 24.9389777}
 
-response = client.send_request(headers, json.dumps(requestBody).encode('utf-8'))
+response = client.send_request(headers, json.dumps(requestBody).encode("utf-8"))
 print(response)
 
 input()
 
 """
+3)
 Send a request for road maps including a trace of GPS locations from the HTTP client to the
 server. The server returns a regional road map for the first GPS location in the trace
 immediately. After that, the server pushes the regional maps corresponding to the following
@@ -32,5 +34,20 @@ your client can decide when to fetch the next map one by one based on the curren
 of the vehicle, please configure the moving speed and demonstrate that function. 
 """
 
+headers = [
+    (":method", "GET"),
+    (":path", "/getMaps"),
+    (":authority", "localhost"),
+    (":scheme", "https"),
+]
 response = client.send_request(headers, b"data 2")
 print(response)
+
+
+"""
+4)
+Select a GPS location and its corresponding photo from the materials we provide. Send the
+photo with GPS location to the server. Show that the server has received the data
+successfully. If you have implemented the feature in two ways (POST vs. PUT), please
+demonstrate both. 
+"""
